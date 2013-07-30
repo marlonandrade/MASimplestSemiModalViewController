a6169381e875970b3567e88f852dcb6069ec8bc6
Marlon Andrade <marlonmandrade@gmail.com>
2013-07-30 18:32:35 -0300

    creating demo project

---
+++ MASimplestSemiModelViewControllerDemo/MASimplestSemiModelViewControllerDemo/MAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAAppDelegate.h
+//  MASimplestSemiModelViewControllerDemo
+//
+//  Created by Marlon Andrade on 30/07/13.
+//  Copyright (c) 2013 Marlon Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MASimplestSemiModelViewControllerDemo/MASimplestSemiModelViewControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MASimplestSemiModelViewControllerDemo
+//
+//  Created by Marlon Andrade on 30/07/13.
+//  Copyright (c) 2013 Marlon Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAAppDelegate class]));
+    }
+}
